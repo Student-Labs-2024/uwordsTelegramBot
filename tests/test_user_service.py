44bb300043cd60942390b41309f3965a3478b9fe
AubakirovAzamat <azamat.aubakirov.00@mail.ru>
2024-08-21 08:08:01 +0600

    feat: add tests

---
+++ tests/test_user_service.py
@@ -0,0 +1,44 @@
+import pytest
+from unittest.mock import AsyncMock, patch
+from src.database.models import User
+from src.services.user_service import create_user, get_user
+
+
+class TestUserService:
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("telegram_id, app_id", [(123456, 789), (987654, 321)])
+    async def test_create_user(self, telegram_id, app_id):
+        with patch(
+            "src.services.user_service.async_session_maker"
+        ) as mock_session_maker:
+            mock_session = AsyncMock()
+            mock_session_maker.return_value.__aenter__.return_value = mock_session
+
+            result = await create_user(telegram_id, app_id)
+
+            assert result is True
+
+            mock_session.execute.assert_called_once()
+            mock_session.commit.assert_called_once()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("telegram_id", [123456, 987654])
+    async def test_get_user(self, telegram_id):
+        with patch(
+            "src.services.user_service.async_session_maker"
+        ) as mock_session_maker:
+            mock_session = AsyncMock()
+            mock_session_maker.return_value.__aenter__.return_value = mock_session
+
+            mock_result = AsyncMock()
+            mock_result.scalar_one_or_none.return_value = User(
+                tg_user_id=telegram_id, main_api_user_id=123, notice=True
+            )
+            mock_session.execute.return_value = mock_result
+
+            result = await get_user(telegram_id)
+
+            assert result is not None
+
+            mock_session.execute.assert_called_once()
